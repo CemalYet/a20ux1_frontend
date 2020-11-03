b68c05e10511adacf55dc2081902572f130fed8a
seppe.fleerackers <seppe.fleerackers@student.kuleuven.be>
2020-11-03 17:08:32 +0100

    Updating feed page layout and look

--- src/pages/feed/App.vue
+++ src/pages/feed/App.vue
@@ -1,88 +1,136 @@
 <template>
   <v-app>
+    <!--Bar at the top of the page-->
     <v-app-bar
         hide-on-scroll
         absolute
         color="white"
         scroll-target="#scrolling-techniques-4"
+        elevation="1"
     >
-      <v-app-bar-nav-icon
-          @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
 
+      <!--Hamburger menu-->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+        <v-icon large color="black">mdi-menu</v-icon>
+        <v-badge color="red" :content="notifications"></v-badge>
+      </v-app-bar-nav-icon>
+
+      <!-- Page title -->
+      <v-spacer></v-spacer>
       <v-toolbar-title>snAPP</v-toolbar-title>
-
       <v-spacer></v-spacer>
 
+      <!--Profile icon-->
       <v-btn icon>
-        <v-icon>mdi-account-outline</v-icon>
+        <v-icon large color="black">mdi-account-outline</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <!--Page-->
     <v-main>
+      <!--Afbeeldingen om het scherm ff mee te vullen, zodat ik het scrollen kon testen-->
+      <template>
+        <v-row>
+          <v-col
+              v-for="n in 9"
+              :key="n"
+              class="d-flex child-flex"
+              cols="4"
+          >
+            <v-img
+                :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                aspect-ratio="1"
+                class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                >
+                  <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-col>
+        </v-row>
+      </template>
+
+      <!--Navigation menu on the left side-->
       <v-navigation-drawer
           v-model="drawer"
-          absolute
+          fixed
           temporary
       >
         <v-list
             nav
             dense
         >
-          <v-list-item-group
-              v-model="group"
-              active-class="deep-purple--text text--accent-4"
-          >
-            <v-list-item>
-              <v-list-item-title>Foo</v-list-item-title>
-            </v-list-item>
+          <!--Avatar + name-->
+          <v-list-item two-line>
+            <v-list-item-avatar size="70">
+              <v-img :src="avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>Seppe Fleerackers</v-list-item-title>
+              <v-list-item-subtitle>@seppe.f</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
 
-            <v-list-item>
-              <v-list-item-title>Bar</v-list-item-title>
-            </v-list-item>
-
-            <v-list-item>
-              <v-list-item-title>Fizz</v-list-item-title>
-            </v-list-item>
-
-            <v-list-item>
-              <v-list-item-title>Buzz</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
+          <!--Menu options-->
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon large color="green">mdi-tree-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Discoveries</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon large color="green">mdi-account-plus-outline</v-icon>
+              <v-badge color="red" :content="notifications" overlap></v-badge>
+            </v-list-item-icon>
+            <v-list-item-title>Friends</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon large color="green">mdi-shield-star-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Badges</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon large color="green">mdi-map-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Map</v-list-item-title>
+          </v-list-item>
         </v-list>
+
+        <!-- Settings button-->
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn text>
+              <v-icon large color="green" left>mdi-cog-outline</v-icon>
+              Settings
+            </v-btn>
+          </div>
+        </template>
       </v-navigation-drawer>
 
-      <v-bottom-navigation
-          absolute
-          hide-on-scroll
-          horizontal
-          scroll-target="#hide-on-scroll-example"
-      >
-        <v-btn
-            color="deep-purple accent-4"
-            text
-        >
-          <span>Recents</span>
 
-          <v-icon>mdi-history</v-icon>
+      <!--Navigation menu at the bottom-->
+      <v-bottom-navigation fixed grow>
+        <v-btn color="white">
+          <v-icon large color="green">mdi-shield-star-outline</v-icon>
         </v-btn>
-
-        <v-btn
-            color="deep-purple accent-4"
-            text
-        >
-          <span>Favorites</span>
-
-          <v-icon>mdi-heart</v-icon>
+        <v-btn x-large color="black">
+          <v-icon x-large color="white">mdi-camera-plus-outline</v-icon>
         </v-btn>
-
-        <v-btn
-            color="deep-purple accent-4"
-            text
-        >
-          <span>Nearby</span>
-
-          <v-icon>mdi-map-marker</v-icon>
+        <v-btn color="white">
+          <v-icon large color="green">mdi-map-outline</v-icon>
         </v-btn>
       </v-bottom-navigation>
     </v-main>
@@ -95,15 +143,16 @@
 export default {
   name: 'App',
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
     drawer: false,
     group: null,
+    notifications: 2,
+    avatar: "https://scontent-bru2-1.xx.fbcdn.net/v/t31.0-8/27907755_964224010401572_4566376548678829171_o.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=2wrEVoQrdBkAX9MBLOP&_nc_ht=scontent-bru2-1.xx&oh=81c5c254570b087bda35d1ced5624cac&oe=5FC6E541",
   }),
   watch: {
-    group () {
+    group() {
       this.drawer = false
     },
   },
