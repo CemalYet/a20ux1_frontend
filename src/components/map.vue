78ae46556ade91e86f96698c4a902377cfffab5a
seppe.fleerackers <seppe.fleerackers@student.kuleuven.be>
2020-11-17 21:51:56 +0100

    some lay out stuff

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,24 +1,11 @@
 <template>
   <div>
-    <div id="top_nav">
-      <v-text-field
-          solo
-          label="Search"
-          clearable></v-text-field>
-      <v-chip-group
-          mandatory
-          active-class="--main-color">
-        <v-chip @click="getMyDiscoveries">Mine</v-chip>
-        <v-chip @click="getFriendsDiscoveries">Friends</v-chip>
-        <v-chip @click="getPopularDiscoveries">Popular</v-chip>
-      </v-chip-group>
-      <router-view name="backButton"></router-view>
-    </div>
-    <div id="discovery_map">
+    <div id="Discovery_map">
       <GmapMap
           :center=getMapCenter
           :zoom="12"
           style="width:100vw; height:100vh;"
+          :options="mapOptions"
       >
         <gmap-custom-marker
             :key="index"
@@ -31,9 +18,26 @@
       </GmapMap>
     </div>
 
+    <div id="Search_box">
+      <div id="Back_button">
+        <router-view name="backButton"></router-view>
+      </div>
+      <v-text-field
+          solo
+          label="Search"
+          clearable>
+      </v-text-field>
+    </div>
+    <div id="Buttons">
+      <v-btn @click="getMyDiscoveries" rounded small>Mine</v-btn>
+      <v-btn @click="getFriendsDiscoveries" rounded small>Friends</v-btn>
+      <v-btn @click="getPopularDiscoveries" rounded small>Popular</v-btn>
+    </div>
+
+
     <router-link to="/">
-      <v-bottom-sheet id="Disco_info"
-           v-model="updateMarkerDiscoveryOverlay"
+      <div id="Disco_info"
+           v-if="updateMarkerDiscoveryOverlay"
       >
         <v-sheet
             class="mx-auto"
@@ -41,24 +45,26 @@
             max-width="100vw"
         >
           <v-slide-group
-              v-if="updateMarkerDiscoveryOverlay"
+              v-model="updateMarkerDiscoveryOverlay"
           >
             <v-slide-item
                 v-for="image in getSelectedMarker.images"
                 :key="image"
             >
               <img
+                  class="images"
                   :src="image"
                   alt=""
-                  height="200"
               >
             </v-slide-item>
           </v-slide-group>
-          <h3>Title</h3>
-          <h5>Name - Date</h5>
-          <h5>Location</h5>
+          <div id="images_text">
+            <h3>Zo mooi in bloei!</h3>
+            <h5>Seppe Fleerackers - 17 november 2020</h5>
+            <h5>De Liereman</h5>
+          </div>
         </v-sheet>
-      </v-bottom-sheet>
+      </div>
     </router-link>
   </div>
 </template>
@@ -68,6 +74,14 @@ import GmapCustomMarker from 'vue2-gmap-custom-marker';
 
 export default {
   name: "map",
+
+  data() {
+    return {
+      mapOptions: {
+        disableDefaultUI: true,
+      },
+    };
+  },
 
   components: {
     'gmap-custom-marker': GmapCustomMarker
@@ -82,23 +96,23 @@ export default {
     getMapCenter() {
       return this.$store.getters.getMapCenter;
     },
-    getMarkers(){
+    getMarkers() {
       return this.$store.getters.getMapMarkers;
     },
-    getSelectedMarker:{
-      get(){
+    getSelectedMarker: {
+      get() {
         return this.$store.getters.getSelectedMarker;
       },
-      set(value){
+      set(value) {
         this.$store.commit("updateSelectedMarker", value)
 
       }
     },
-    updateMarkerDiscoveryOverlay:{
-      get(){
+    updateMarkerDiscoveryOverlay: {
+      get() {
         return this.$store.getters.getMarkerDiscoveryOverlay;
       },
-      set(value){
+      set(value) {
         this.$store.commit("updateMarkerDiscoveryOverlay", value)
 
       }
@@ -108,15 +122,15 @@ export default {
   methods: {
     getDiscoInfo(disco) {
       this.$store.commit("updateSelectedMarker", disco);
-      this.$store.commit("updateMarkerDiscoveryOverlay",true);
+      this.$store.commit("updateMarkerDiscoveryOverlay", true);
     },
-    getMyDiscoveries(){
+    getMyDiscoveries() {
       this.$store.dispatch('discoveriesMe');
     },
-    getFriendsDiscoveries(){
+    getFriendsDiscoveries() {
       this.$store.dispatch('discoveriesFriends');
     },
-    getPopularDiscoveries(){
+    getPopularDiscoveries() {
       this.$store.dispatch('discoveriesPopular');
     },
   }
@@ -129,17 +143,54 @@ export default {
   width: auto;
 }
 
-#discovery_map, #top_nav {
+#Discovery_map {
   position: absolute;
 }
 
-#top_nav {
-  z-index: 2;
-  width: 80%;
+#Search_box {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+}
+
+#Back_button {
+  margin-top: 6px;
+}
+
+#Buttons {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: -25px;
+}
+
+#Buttons button {
+  margin: 4px;
 }
 
 #Disco_info {
-  position: relative;
-  top: 400px;
+  position: absolute;
+  bottom: 0;
+  padding: 0 10px 6px;
+  width: 100%;
+}
+
+.images {
+  padding-right: 4px;
+  height: 100px;
+}
+
+#images_text {
+  padding: 0 10px 6px;
+}
+
+@media screen and (min-width: 600px) {
+  #Disco_info {
+    position: absolute;
+    bottom: 0;
+    margin: 0 10px 6px;
+    width: 400px;
+  }
 }
 </style>
