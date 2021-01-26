da69452b48853cb5383c2fa5f848e155b72a3109
marnix.lijnen <marnix.lijnen@student.kuleuven.be>
2021-01-26 17:14:20 +0100

    Added compression

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
@@ -5,7 +6,33 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/'
   : '/',
-  outputDir: '../a20ux1/',
-  assetsDir: 'public',
-  indexPath: 'app/Views/index.php'
+  outputDir: '../a20ux1/app/Views/',
+  assetsDir: '../../public',
+  indexPath: 'index.html',
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: true
+    },
+    compression:{
+      brotli: {
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.plugin('brotli-compression')
+    config.plugin('gzip-compression')
+  }
 }
